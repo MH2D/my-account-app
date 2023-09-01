97381188b688950479892c9bfff20deb24afb652
Ugo Muhieddine <ugo.muhieddine@student-cs.fr>
2023-09-01 19:02:18 +0200

    tests

--- account_app.py
+++ account_app.py
@@ -3,17 +3,6 @@ from st_files_connection import FilesConnection
 
 # Establish a connection to Google Cloud Storage (GCS)
 conn = st.experimental_connection("gcs", type=FilesConnection)
-conn
-# Define the path to your SQLite3 .db file in GCS
-db_file_path = "data-account-app/account_management_copy.db"
+df = conn.read("data_ugo/ugo_expenses.csv", input_format="csv", ttl=600)
 
-# Read the file using the connection
-with conn.read(db_file_path, ) as file:
-    # Perform actions with the file
-    # For example, you can read its content or use it as a SQLite3 database
-    file_content = file.read()
-
-file_content
-
-# streamlit.errors.StreamlitAPIException: Invalid connection 'service_account'. Supported connection classes: {'snowpark': <class 'streamlit.connections.snowpark_connection.SnowparkConnection'>, 'sql': <class 'streamlit.connections.sql_connection.SQLConnection'>}
-
+df
