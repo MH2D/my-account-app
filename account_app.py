3e31f8d5cbdfc88898b36832bda472622ed84d9a
Ugo Muhieddine <ugo.muhieddine@student-cs.fr>
2023-09-01 18:26:17 +0200

    feat: the great secrets data

--- account_app.py
+++ account_app.py
@@ -1,61 +1,11 @@
 import streamlit as st
-from utils.global_utils import (
-    DATA_PATH,
-    CATEGORIES,
-    DB_NAME,
-    start_db
-)
+from st_files_connection import FilesConnection
 
-from utils.expense_recettes_manage import (
-    add_expense,
-    add_recette,
-    view_and_delete_db
-)
-from utils.first_dashboard import (
-    do_altair_overall,
-    plot_current_month
-)
+# Create connection object and retrieve file contents.
+# Specify input format is a csv and to cache the result for 600 seconds.
+conn = st.experimental_connection('gcs', type=FilesConnection)
+df = conn.read("streamlit-bucket/my_.csv", input_format="csv", ttl=600)
 
-
-
-
-
-
-# Main function to switch between pages
-def main():
-    st.title("Daily Spendings App")
-
-    # Create a navigation menu
-    page = st.sidebar.selectbox("Select a page", ["Expenses", "Recettes", "Dashboard"])
-
-    if page == "Expenses":
-        add_tab, modif_tab = st.tabs(['Add new', 'Manage'])
-        with add_tab:
-            add_expense()
-
-        with modif_tab:
-            view_and_delete_db(table_name='expenses')
-
-
-    if page == "Recettes":
-        add_tab, modif_tab = st.tabs(['Add new', 'Manage'])
-        with add_tab:
-            add_recette()
-        
-        with modif_tab:
-            view_and_delete_db(table_name='recettes')
-
-    if page == "Dashboard":
-        overall, my_other = st.tabs(['Overall', 'My other'])
-        with overall:
-            do_altair_overall()
-            plot_current_month()
-        
-        with my_other:
-            # view_and_delete_db(table_name='recettes')
-            pass
-
-
-
-if __name__ == '__main__':
-    main()
+# Print results.
+for row in df.itertuples():
+    st.write(f"{row.Owner} has a :{row.Pet}:")
