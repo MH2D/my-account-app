d598685a04fa9eddccff055ce0b750633c1b5ed0
Ugo Muhieddine <ugo.muhieddine@student-cs.fr>
2023-09-02 08:48:32 +0200

    wtffff

--- account_app.py
+++ account_app.py
@@ -5,6 +5,6 @@ from st_files_connection import FilesConnection
 conn = st.experimental_connection("gcs", type=FilesConnection)
 df = conn.read("data_ugo/ugo_expenses.csv", input_format="csv", ttl=600)
 
-df
+st.write('Que passo')
 conn.write('data_ugo/ugo_exp_2.csv', df)
 st.write('trop biennnn')
