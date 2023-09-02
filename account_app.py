78a293ac450fd4dd1f5dd9d70266f9a27435fa19
Ugo Muhieddine <ugo.muhieddine@student-cs.fr>
2023-09-02 08:45:31 +0200

    writing to gcs

--- account_app.py
+++ account_app.py
@@ -6,3 +6,5 @@ conn = st.experimental_connection("gcs", type=FilesConnection)
 df = conn.read("data_ugo/ugo_expenses.csv", input_format="csv", ttl=600)
 
 df
+conn.write('data_ugo/ugo_exp_2.csv', df)
+st.write('trop biennnn')
