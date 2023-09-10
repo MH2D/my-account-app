68a9452b2d95ecbc2fcb17d72fa37ddd9bbf0b68
Ugo Muhieddine <ugo.muhieddine@student-cs.fr>
2023-09-10 21:30:54 +0200

    authentication

--- account_app.py
+++ account_app.py
@@ -1,21 +1,46 @@
 
 import streamlit as st
 import authanticated_run as app_run
-username = st.text_input("Username", value="")
 
-pwd = st.text_input("Password", value="", type="password")
+# Define a session state object
+class SessionState:
+    def __init__(self):
+        self.logged_in = False
 
-if st.button("Sign in"):
-    try:
-        secret_pwd = st.secrets[username].password
-        if secret_pwd == pwd:
-            error = False
-        else:
+# Initialize the session state
+session_state = SessionState()
+
+# Create a login page
+def login_page():
+    username = st.text_input("Username", value="")
+
+    pwd = st.text_input("Password", value="", type="password")
+
+    if st.button("Sign in"):
+        try:
+            secret_pwd = st.secrets[username].password
+            if secret_pwd == pwd:
+                error = False
+            else:
+                error = True
+        except:
             error = True
-    except:
-        error = True
 
-    if error:      
-        st.error('Wrong username or password', icon="🚨")
+        if error:      
+            st.error('Wrong username or password', icon="🚨")
+        else:
+            session_state.logged_in = True
+
+# Function to logout
+def logout():
+    session_state.logged_in = False
+
+def main():
+    if not session_state.logged_in:
+        login_page()
     else:
+        st.sidebar.button("Logout", on_click=logout)
         app_run.main()
+
+if __name__ == "__main__":
+    main()
