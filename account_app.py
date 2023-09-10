831d7c4138e9950085bd759368c3871295a97cfc
Ugo Muhieddine <ugo.muhieddine@student-cs.fr>
2023-09-10 21:21:09 +0200

    authentication

--- account_app.py
+++ account_app.py
@@ -5,7 +5,8 @@ username = st.text_input("Username", value="")
 
 pwd = st.text_input("Password", value="", type="password")
 
-try:
-    st.write(st.secrets[username])
-except:
-    st.error('Wrong username', icon="🚨")
+if username:
+    try:
+        st.write(st.secrets[username])
+    except:
+        st.error('Wrong username', icon="🚨")
