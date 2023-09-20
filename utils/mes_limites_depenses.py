a2f35a64653cca57314b459e1a0b52c92b7dc0b7
Ugo Muhieddine <ugo.muhieddine@student-cs.fr>
2023-09-20 19:33:23 +0200

    feat: budget limits

--- utils/mes_limites_depenses.py
+++ utils/mes_limites_depenses.py
@@ -7,19 +7,16 @@ from utils.global_utils import *
 
 def questionnaire_category(USERNAME):
     category_list = list(CATEGORIES['expenses'].keys())
-
+    limits_df = read_csv_from_gcs(f'{USERNAME}_budget_limits.csv', bucket_name=BUCKET_NAME)
+    limits_df['category'] = category_list
+    
     limit_categories = {}
     for cat in category_list:
         limit_categories[cat] = st.number_input(cat, min_value=5)
-    
-    st.table(limits_df)
-    
+        
     if st.button('Update your budget limits'):
-        limits_df = read_csv_from_gcs(f'{USERNAME}_budget_limits.csv', bucket_name=BUCKET_NAME)
-        limits_df['category'] = category_list
 
         for cat, lim in limit_categories.items():
             limits_df.limit = limits_df.category.apply(lambda x: lim)
 
         write_csv_to_gcs(limits_df, f'{USERNAME}_budget_limits.csv', bucket_name=BUCKET_NAME)
-
