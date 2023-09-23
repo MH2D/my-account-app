77a752d33e983b1767ced7a12ea656cf563cc6bb
Ugo Muhieddine <ugo.muhieddine@student-cs.fr>
2023-09-23 12:34:39 +0200

    feat: limit plots

--- utils/mes_limites_depenses.py
+++ utils/mes_limites_depenses.py
@@ -23,8 +23,7 @@ def questionnaire_category(USERNAME):
         
     if st.button('Update your budget limits'):
         for cat, lim in limit_categories.items():
-            limits_df.limit = limits_df.category.apply(lambda x: limit_categories[x])
-
+            limits_df.limit = limits_df.category.apply(lambda x: lim)
         write_csv_to_gcs(limits_df, f'{USERNAME}_budget_limits.csv', bucket_name=BUCKET_NAME)
 
     st.table(limits_df)
