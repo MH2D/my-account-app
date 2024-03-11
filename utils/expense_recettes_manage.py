d3fcdde28c24b74cccbda6be85569b9f5db204fe
Ugo Muhieddine <ugo.muhieddine@student-cs.fr>
2024-03-11 22:10:26 +0100

    feat: sorting

--- utils/expense_recettes_manage.py
+++ utils/expense_recettes_manage.py
@@ -85,7 +85,7 @@ def view_and_delete_db(table_name, USERNAME):
             with_deletion_data_df = data_df[~data_df.id.isin(data_to_delete)]
             write_csv_to_gcs(with_deletion_data_df, csv_filename)
             data_df = read_csv_from_gcs(csv_filename)
-        st.table(data_df)
+        st.table(data_df.sort_values('date', ascending=False))
 
     else:
         st.info(f"No {table_name} recorded yet.")
